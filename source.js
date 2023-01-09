cc411711eaa4a8c76e05cc5108c8c9609d7eab7d
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2023-01-09 15:14:06 -0500

    Final Non-Major Change (for now)
    Another keyword, other changes to *attempt* to stop trackers (Trackers are a pain in userscripts, may make an extension)

--- source.js
+++ source.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extremely BAD Adblock
-// @version      1.8
-// @description  Really bad and wont stop tracking (Besides *some* google trackers) but bypasses where real adblocks fail <3 | Based on generic attributes of ad elements, rather than advanced filterlists, and only deletes elements after loading the page rather than blocking them instantly (Has some false positives, but will work on every common website)
+// @version      1.9
+// @description  Really bad and wont stop tracking very well but bypasses where real adblocks fail <3 | Based on generic attributes of ad elements, rather than advanced filterlists, and only deletes elements after loading the page rather than blocking them instantly (Has some false positives, but will work on every common website)
 // @author       Kaid#0001
 // @run-at       document-start
 // @match        *://*/*
@@ -49,21 +49,19 @@
     {
         elementsArrayUwU = Array.from(document.all);
         elementsArrayUwU.forEach(el => {
-            if (check(el,"advert") || check(el,"adsense") || check(el,"tracker") || check(el,"interad") || check(el,"interstitial") || (check(el,"google") && check(el,"ad") && !check(el,"captcha") && !check(el,"login")) || check(el,"bncloud") || check(el,"referral") || check(el,"refid") | check(el,"trafficjunky") || check(el,"pb_template") || check(el,"__clb")){
+            if (check(el,"advert") || check(el,"affiliate") || check(el,"doubleclick.net") || check(el,"adsense") || check(el,"tracker") || check(el,"interad") || check(el,"interstitial") || (check(el,"google") && check(el,"ad") && !check(el,"captcha") && !check(el,"login")) || check(el,"bncloud") || check(el,"referral") || check(el,"refid") | check(el,"trafficjunky") || check(el,"pb_template") || check(el,"__clb")){
                 el.remove();
                 counter++;
             }
-            if (el.tagName === "SCRIPT") {
-                if (el.innerHTML != el.innerHTML.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url")) {
-                    el.innerHTML = el.innerHTML.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
-                    counter++;
-                }
-                if (el.src != el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url")) {
-                    el.src = el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
-                    counter++;
-                }
+            if (el.innerHTML != el.innerHTML.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url")) {
+                el.innerHTML = el.innerHTML.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
+                counter++;
             }
-        })
+            if (el.src && el.src != el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url")) {
+                el.src = el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
+                counter++;
+            }
+        });
         elementsArrayUwU = Array.from(document.getElementsByTagName('iframe')).concat(Array.from(document.getElementsByTagName('img'))).concat(Array.from(document.getElementsByTagName('a')));
         elementsArrayUwU.forEach(el => {
             if (el.getAttribute('data-jsarwt')){
@@ -80,11 +78,7 @@
                     counter++;
                 }
             }
-            if (el.tagName === "IMG" && el.src != el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url")) {
-                el.src = el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
-                counter++;
-            }
-        })
+        });
         Array.from(document.getElementsByTagName('div')).forEach(el => { // Anti Pornhub WITH support for non-bypassing adblockers (they use a special ad format if an adblocker is detected, my adblocker isnt detected, but if u are using two adblocks, this will catch the new ads made by ph)
             if(el.getAttribute('class') && el.getAttribute('class').toLowerCase().includes("adlinks")){
                 el.parentElement.remove();
@@ -96,9 +90,9 @@
                         el.remove();
                         counter++;
                     }
-                })
+                });
             }
-        })
+        });
         if (counter > lastCounter) {
             console.log("EBA: Blocked Items | " + counter);
             lastCounter = counter;
