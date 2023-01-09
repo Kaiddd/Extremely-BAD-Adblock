c46cc7dcc52a95b64e4cd750e97326e287fd08b9
Kaiddd <37759450+Kaiddd@users.noreply.github.com>
2023-01-09 07:56:39 -0500

    Bam. Google tracking go bye bye (sometimes)

--- source.js
+++ source.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Extremely BAD Adblock
-// @version      1.6
+// @version      1.7
 // @description  Really bad and wont stop tracking (Besides *some* google trackers) but bypasses where real adblocks fail <3 | Based on generic attributes of ad elements, rather than advanced filterlists, and only deletes elements after loading the page rather than blocking them instantly (Has some false positives, but will work on every common website)
 // @author       Kaid#0001
 // @run-at       document-start
@@ -9,8 +9,6 @@
 
 (function() {
     'use strict';
-    //removing till I can fix a weird bug
-    //document.documentElement.innerHTML = document.documentElement.innerHTML.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"google ad lmao");
 
     let elementsArrayUwU;
 
@@ -51,6 +49,10 @@
             if (check(el,"advert") || check(el,"adsense") || check(el,"tracker") || check(el,"interad") || check(el,"interstitial") || (check(el,"google") && check(el,"ad") && !check(el,"captcha") && !check(el,"login")) || check(el,"bncloud") || check(el,"referral") || check(el,"refid") | check(el,"trafficjunky") || check(el,"pb_template") || check(el,"__clb")){
                 el.remove();
             }
+            if (el.tagName === "SCRIPT") {
+                el.innerHTML = el.innerHTML.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
+                el.src = el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
+            }
         })
         elementsArrayUwU = Array.from(document.getElementsByTagName('iframe')).concat(Array.from(document.getElementsByTagName('img'))).concat(Array.from(document.getElementsByTagName('a')));
         elementsArrayUwU.forEach(el => {
@@ -64,6 +66,9 @@
                 if (el) {
                     el.remove();
                 }
+            }
+            if (el.tagName === "IMG") {
+                el.src = el.src.replace(/googletagmanager|google-analytics|googleanalytics|googleadservices|adwords|adsense|admob|urchin.com|doubleclick.net/gim,"thisIsAGoogleAd.invalid.url");
             }
         })
         Array.from(document.getElementsByTagName('div')).forEach(el => { // Anti Pornhub WITH support for non-bypassing adblockers (they use a special ad format if an adblocker is detected, my adblocker isnt detected, but if u are using two adblocks, this will catch the new ads made by ph)
